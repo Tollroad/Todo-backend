cc5f7907300841a8e0267a8ba08d1757ff0e3ff8
Tollroad <nimits24@gmail.com>
2023-05-26 12:16:26 +0530

    domain add on cookie

--- utils/features.js
+++ utils/features.js
@@ -11,6 +11,7 @@ export const sendCookie = (findUser,res,message,statusCode=200)=>{
         maxAge:1000*60*10,
         sameSite:process.env.NODE_ENV === "development" ? "lax": "none",
         secure:process.env.NODE_ENV === "development" ? false : true, 
+        domain:".onrender.com"
       }).json({
         success:true,
         message:message
